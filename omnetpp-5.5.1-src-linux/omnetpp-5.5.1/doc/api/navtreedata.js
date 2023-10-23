639781e180068ed450a0c48f83eb49597cd80bef
prototypedave <popupgfj@gmail.com>
2023-10-23 17:38:54 +0300

    first commit

---
+++ omnetpp-5.5.1-src-linux/omnetpp-5.5.1/doc/api/navtreedata.js
@@ -0,0 +1,45 @@
+var NAVTREE =
+[
+  [ "OMNeT++ Simulation Library", "index.html", [
+    [ "OMNeT++ API Reference", "index.html", null ],
+    [ "API Changes", "APIChanges.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classomnetpp_1_1cArcFigure.html#aeb14ddb76d4dfa0e7ae20b529b856081",
+"classomnetpp_1_1cCommBuffer.html#abe0cf9231776b56cae8e5b54786618fc",
+"classomnetpp_1_1cDefaultMessagePrinter.html#a3afff24fcfd8a70d7b826b947a6ce2e6",
+"classomnetpp_1_1cEvent.html#a9bbbedfec839d46811972a668fffb6fb",
+"classomnetpp_1_1cHistogram.html#a1d5e644aed9d2ebe5daa3a4fac00a1ca",
+"classomnetpp_1_1cLegacyHistogramBase.html#a95a8f0e887662738dfdce02cbbd35f74",
+"classomnetpp_1_1cMsgPar.html#a9331c3cdb698999edbb0431a48286efb",
+"classomnetpp_1_1cObject.html#a32f73a8adf0bf38a045afcbd9babbd05",
+"classomnetpp_1_1cParImpl.html#a19f70c70aa0a32610cb34986d949e0fc",
+"classomnetpp_1_1cPreModuleReparentNotification.html",
+"classomnetpp_1_1cScheduler.html#a99d94cf2c52f0f6ec1092691d54b2ba5",
+"classomnetpp_1_1cTimestampedValue.html",
+"classomnetpp_1_1opp__string__map.html",
+"group__RegMacros.html#gafb4652c1aa744a704decd8bf8495badd"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
